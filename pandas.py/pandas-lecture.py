d0ca9a5b5637cb8532f6424413bf32f13f9bbd68
Sagar Bhattarai <sagar.bhattarai2062@gmail.com>
2026-06-24 14:43:29 +0545

    learned dataframe and saving dataframe into files in pandas

---
+++ pandas.py/pandas-lecture.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#  read data from a CSV file into a dataframe
+
+# df=pd.read_csv('sample.csv', encoding='utf-8')
+# print(df)
+
+
+# creating a dataframe and saving into file
+
+# data={
+#     'Name':['sagar','alice','bob'],
+#     'Age':[20,25,22],
+#     'City':['butwal','kathmandu','bharatput']
+# }
+
+# df=pd.DataFrame(data)
+# print(df)
+
+# saving dataframe into file
+# df.to_csv('output.csv',index=False) # CSV file
+# df.to_excel('output.xlsx',index=False) # excel file
+# df.to_json('output.json',index=False) # JSON file
+
+
